8ee543ec615f698e7d35c9fd48df0db357df4e58
David Larpent <david@getlavanda.com>
2019-10-13 15:10:58 +0100

    Fix netlify issues

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,11 +4,9 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
