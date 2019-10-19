2779b463fc636c4ddf4b160239ab42ee14d00f5d
David Larpent <david@getlavanda.com>
2019-10-19 16:50:44 +0100

    Add sitemap generator

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    `gatsby-plugin-sitemap`,
     'gatsby-plugin-sass',
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
