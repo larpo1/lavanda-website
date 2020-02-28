5d1e5587cba369c3a799ce8bf0d3c0cb4b503a99
David Larpent <david@getlavanda.com>
2020-02-28 18:59:58 +0000

    Add bamboo widget

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -10,12 +10,10 @@ const CareersPage = () => (
     <Nav />
       <div className={"hero-body"}>
         <div className={"container has-margin-top-100"}>
-        <div id="BambooHR"><script src="https://lavanda.bamboohr.com/js/jobs2.php" type="text/javascript"></script><div id="BambooHR-Footer">Powered by<a href="http://www.bamboohr.com" target="_blank" rel="noopener external nofollow noreferrer"><img src="https://resources.bamboohr.com/images/footer-logo.png" alt="BambooHR - HR software"/></a></div></div>
-
-        {/* <div id="BambooHR">
-          <object data="https://lavanda.bamboohr.com/jobs/embed2.php" title="Bamboo" className="bamboo-embed">
+        <div id="BambooHR">
+          <object data="https://lavanda.bamboohr.com/jobs/" title="Bamboo" className="bamboo-embed">
           </object>
-        </div> */}
+        </div>
         </div>
       </div>
     </div>
