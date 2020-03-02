bfdc0c4066b61005c5f3e538d93496325184a1ec
David Larpent <david@getlavanda.com>
2020-03-02 15:44:33 +0000

    Fix calendly by using componentWillMount

--- src/components/calendly-book-demo.js
+++ src/components/calendly-book-demo.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react"
-import { InlineWidget } from "react-calendly";
+//import { InlineWidget } from "react-calendly";
 
 export default class Calendly extends Component
 { 
   componentDidMount() {
+  }
+
+  componentWillMount() {
+    const head = document.querySelector('head');
+    const script = document.createElement('script');
+    script.setAttribute('src',  'https://assets.calendly.com/assets/external/widget.js');
+    head.appendChild(script);
   }
 
   componentWillUnmount() {
@@ -11,8 +18,17 @@ export default class Calendly extends Component
 
   render = () => {
     return ( 
-      <div className="container">
-        <InlineWidget url="https://calendly.com/book-lavanda/discovery-call?primary_color=0989d4" />
+      // <div className="container">
+      //   <InlineWidget url="https://calendly.com/book-lavanda/discovery-call?primary_color=0989d4" />
+      // </div>
+
+      <div>
+        <div id="schedule_form">
+          <div 
+            className="calendly-inline-widget"
+            data-url="https://calendly.com/book-lavanda/discovery-call?primary_color=0989d4"
+            style={{ minWidth: '320px', height: '580px' }} />
+        </div>
       </div>
     )
   }
