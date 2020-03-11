d465f78242171dcdbfac5ab1890dacbda5f2cd35
David Larpent <david.larpent@me.com>
2020-03-11 07:59:00 +0000

    Set config.yml to staging

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,6 +66,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
+        manualInit: true,
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
