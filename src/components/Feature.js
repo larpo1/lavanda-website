aefb41309344428fe6dad8a7c7685e30d25a3349
David Larpent <david.larpent@me.com>
2020-03-15 09:12:47 +0000

    Fix filename issue

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,6 +1,7 @@
-import React from "react"
-import { Link } from "gatsby"
-import "../../static/styles/main.scss"
+import React from "react";
+import { Link } from "gatsby";
+import "../../static/styles/main.scss";
+import PreviewCompatibleImage from "../components/PreviewCompatibleImage"
 
 export default props => (
   <section className={"feature-item"}>
@@ -8,29 +9,34 @@ export default props => (
       <div className={"columns " + props.imageAlign}>
         <div className={"column first"}>
           <div className={"Aligner"}>
-            <img
-              src={props.featureImage}
-              className={"feature-image Aligner-item"}
-              alt="Alt text for this one"
-            />
+            <div style={{ width: '100%' }}>
+              <PreviewCompatibleImage
+                imageInfo={{
+                  image: props.featureImage,
+                  alt: props.titleContent
+                }}
+              />
+            </div>
           </div>
         </div>
         <div className={"column"}>
           <div className={"Aligner"}>
-            <div className={"Aligner-item feature-content has-text-centered-mobile"}>
-              <h2 className={"title"}>{props.titleContent}</h2>
-              <p className={"subtitle"}>
-                {props.textContent}
-              </p>
-              { props.linkTarget.length > 0 &&
-              <Link to={props.linkTarget} className={"button is-secondary"}>
-                Learn More &#10511;
-              </Link>
+            <div
+              className={
+                "Aligner-item feature-content has-text-centered-mobile"
               }
+            >
+              <h2 className={"title"}>{props.titleContent}</h2>
+              <p className={"subtitle"}>{props.textContent}</p>
+              {props.linkTarget && props.linkTarget.length ? (
+                <Link to={props.linkTarget} className={"button is-secondary"}>
+                  Learn More &#10511;
+                </Link>
+              ) : null}
             </div>
           </div>
         </div>
       </div>
     </div>
   </section>
-)
+);
