21ab0ef76c9f5883bf1f3d40f0844c60cfa815da
David Larpent <david.larpent@me.com>
2020-03-24 00:41:13 +0000

    Add new landing page content

---
+++ src/pages/lp/lavanda-offer.md
@@ -0,0 +1,75 @@
+---
+templateKey: landing-page
+url: lavanda-offer
+title: We've got you covered
+hero:
+  lang: html
+  code: >-
+
+    <p class="subtitle has-margin-top-20">In support of our industry, we are
+    giving away our award-winning Lavanda PMS completely free-of-charge for the
+    next 3 months. Speak to us today and find out how to reduce your operating
+    expenses now.</p>
+
+    <a class="button is-primary is-medium has-margin-right-10"
+    href="/book-a-demo">Talk to Us</a>
+
+    <a class="button is-medium" href="#video">Learn More</a>
+bgImage: ''
+fgImage: /img/dashboard-mirrored.png
+heroLightOrDark: is-white
+heroSize: is-fullheight
+awards:
+  - awardTitle: 'WINNER: Best Short-term Rental Platform 2020'
+    awardImage: /img/awards-saa-2020.png
+    awardImageAlt: 'WINNER: Best Short-term Rental Platform 2020'
+    awardURLTarget: ''
+  - awardTitle: 'WINNER: Best Property Management Solution 2020'
+    awardImage: /img/awards-shortz-2020.jpg
+    awardImageAlt: 'WINNER: Best Property Management Solution 2020'
+videoSection:
+  title: See how we can help
+  topText: >-
+    Watch the video and see what you will be receiving free-of-charge for the
+    next 3 months. 
+  vimeoURL: 'https://player.vimeo.com/video/371912890'
+  bottomText: ''
+highlights:
+  - titleContent: CRM
+    textContent: >-
+      Lavanda PMS provides you with a fully integrated CRM and tools to help
+      manage your owner sales funnel. Save time and effort in finding customers,
+      managing contacts and automating followups. We make staying in contact,
+      easy.
+    linkTarget: ''
+    imageAlign: img-left
+    image: /img/crm.png
+  - titleContent: Workforce Management
+    textContent: >-
+      Easily add task automation to generate cleans, schedule linen drops,
+      check-ins, maintenance, driver and runner tasks. Our PMS even knows the
+      difference between Owner-Return, Guest-Ready, Mid-Stay and Owner-Leaving
+      Cleans.
+    imageAlign: img-right
+    image: /img/flex-task-completed.png
+  - textContent: >-
+      Manage all maintenance, onboarding, repair, guest and owner request issues
+      from our state-of-the-art ticket management tool. Our ticket system
+      integrates with your entire property portfolio, keeping all of the
+      information you need ready at-a-glance.
+    titleContent: Ticket System
+    imageAlign: img-left
+    image: /img/operations.png
+  - titleContent: Valuations
+    textContent: >-
+      Capture leads through valuations on your website, or conduct a valuation
+      on a new lead directly in the PMS and store it. Draw on our dataset of
+      millions of short term rental properties worldwide, including AirDNA,
+      Transparent and our own proprietary datasets.
+    imageAlign: img-right
+    image: /img/valuations.png
+tags:
+  - Awards
+  - Corona
+description: test meta
+---
--- src/templates/landing-page.js
+++ src/templates/landing-page.js
@@ -6,6 +6,7 @@ import { graphql, Link } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
 import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
+import VideoSection from "../components/VideoSection";
 import Feature from "../components/Feature";
 import logo from "../../static/img/lavanda-logo.svg";
 
@@ -23,38 +24,40 @@ export const LandingPageTemplate = ({
   bgImage,
   fgImage,
   awards,
-  heroLightOrDark
+  heroLightOrDark,
+  videoSection
 }) => {
   const LandingContent = contentComponent || Content;
 
   return (
     <Layout>
       <section
-        className={heroLightOrDark + " hero overlay " + heroSize}
+        className={heroLightOrDark + " hero wave-container overlay " + heroSize}
         style={{
           backgroundImage: `url(${
             !!bgImage ? bgImage.childImageSharp.fluid.src : bgImage
           })`,
-          backgroundSize: `cover`,
-          backgroundPosition: `center center`
+          backgroundRepeat: `no-repeat`,
+          backgroundPosition: `top -200px right -300px`,
+          backgroundSize: `110vh`
         }}
       >
         <div className="hero-head ">
-          <div className="container has-text-centered">
-          <Link to="/" className={"navbar-item"}>
-            <div className={"logo"}>
-              <img alt={"Logo"} src={logo} />
-            </div>
-          </Link>
+          <div className="container">
+            <Link to="/" className={""}>
+              <div className={"logo has-margin-top-40 has-text-centered"}>
+                <img alt={"Logo"} src={logo} width="200" />
+              </div>
+            </Link>
           </div>
         </div>
         <div className="hero-body">
           {helmet || ""}
 
-          <div className="container content">
-            <div className="columns">
+          <div className="container content" style={{ marginTop: "-140px" }}>
+            <div className="columns is-vcentered">
               <div className="column is-half">
-                <h1 className="title is-size-2 has-text-weight-bold is-bold-light has-text-centered ">
+                <h1 className="title is-size-1 has-text-weight-bold is-bold-light has-text-centered ">
                   {title}
                 </h1>
 
@@ -82,43 +85,78 @@ export const LandingPageTemplate = ({
         </div>
 
         <div className={"hero-foot"}>
-        <div className={"container has-text-centered-mobile"}>
-          {awards && awards.length ? (
-            <div className="awards has-padding-40-mobile">
-              {awards.map((award, index) => (
-                <div className="award" alt={award.awardTitle} key={index}>
-                  {award ? (
-                    <PreviewCompatibleImage
-                      imageInfo={{
-                        image: award.awardImage,
-                        alt: award.awardImageAlt
-                      }}
-                    />
-                  ) : null}
-                  <p className="award-title">{award.awardTitle}</p>
-                </div>
-              ))}
-            </div>
-          ) : null}
-        </div>
-      </div>
-
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <defs>
+            <linearGradient id="myGradient" gradientTransform="rotate(0)">
+              <stop offset="0%" stopColor="#11253b" />
+              <stop offset="50%" stopColor="#183656" />
+              <stop offset="100%" stopColor="#477fba" />
+            </linearGradient>
+          </defs>
           <path
-            fill="#ffffff"
-            fillOpacity="0.9"
-            d="M0,192L80,213.3C160,235,320,277,480,272C640,267,800,213,960,192C1120,171,1280,181,1360,186.7L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+            fill="url('#myGradient')"
+            gradientTransform="rotate(180)"
+            fillOpacity="0.5"
+            d="M0,288L80,282.7C160,277,320,267,480,250.7C640,235,800,213,960,208C1120,203,1280,213,1360,218.7L1440,224L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
           ></path>
           <path
-            fill="#ffffff"
-            fillOpacity="0.5"
-            d="M0,224L80,213.3C160,203,320,181,480,192C640,203,800,245,960,272C1120,299,1280,309,1360,314.7L1440,320L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+            fill="url('#myGradient')"
+            gradientTransform="rotate(180)"
+            fillOpacity="0.7"
+            d="M0,320L80,288C160,256,320,192,480,192C640,192,800,256,960,282.7C1120,309,1280,299,1360,293.3L1440,288L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          ></path>
+          <path
+            fill="url('#myGradient')"
+            gradientTransform="rotate(180)"
+            fillOpacity="0.2"
+            d="M0,288L80,288C160,288,320,288,480,272C640,256,800,224,960,229.3C1120,235,1280,277,1360,298.7L1440,320L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          ></path>
+          <path
+            fill="url('#myGradient')"
+            gradientTransform="rotate(180)"
+            fillOpacity="0.2"
+            d="M0,256L80,224C160,192,320,128,480,133.3C640,139,800,213,960,240C1120,267,1280,245,1360,234.7L1440,224L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          ></path>
+          <path
+            fill="url('#myGradient')"
+            gradientTransform="rotate(180)"
+            fillOpacity="1"
+            d="M0,192L80,181.3C160,171,320,149,480,160C640,171,800,213,960,224C1120,235,1280,213,1360,202.7L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
           ></path>
         </svg>
+          <div className={"container has-text-centered-mobile"}>
+            {awards && awards.length ? (
+              <div className="awards has-padding-40-mobile">
+                {awards.map((award, index) => (
+                  <div className="award" alt={award.awardTitle} key={index}>
+                    {award ? (
+                      <PreviewCompatibleImage
+                        imageInfo={{
+                          image: award.awardImage,
+                          alt: award.awardImageAlt
+                        }}
+                      />
+                    ) : null}
+                    <p className="award-title">{award.awardTitle}</p>
+                  </div>
+                ))}
+              </div>
+            ) : null}
+          </div>
+        </div>
+
+       
       </section>
 
+      <VideoSection
+        title={videoSection.title}
+        topText={videoSection.topText}
+        vimeoURL={videoSection.vimeoURL}
+        bottomText={videoSection.bottomText}
+      />
+
       {highlights && highlights.length ? (
-        <div>
+        <section>
           {highlights.map(hl => (
             <Feature
               key={hl}
@@ -129,7 +167,7 @@ export const LandingPageTemplate = ({
               featureImage={hl.image}
             />
           ))}
-        </div>
+        </section>
       ) : null}
 
       <section>
@@ -203,6 +241,7 @@ const LandingPage = ({ data }) => {
       highlights={lp.frontmatter.highlights}
       awards={lp.frontmatter.awards}
       heroLightOrDark={lp.frontmatter.heroLightOrDark}
+      videoSection={lp.frontmatter.videoSection}
     />
   );
 };
@@ -257,6 +296,12 @@ export const pageQuery = graphql`
           awardURLTarget
           awardImageAlt
           awardTitle
+        }
+        videoSection {
+          title
+          topText
+          vimeoURL
+          bottomText
         }
         highlights {
           titleContent
