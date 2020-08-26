f55d3c33b3b39d16101e5ba90e6fa66d6cea7399
David Larpent <david.larpent@me.com>
2020-08-26 16:37:37 +0100

    Clean up

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -84,7 +84,7 @@ export const IndexPageTemplate = ({
         </div>
       </div>
 
-      <svg xmlns="http://www.w3.org/2000/svg" className="rising-wave" viewBox="0 0 1440 320">
+      {/* <svg xmlns="http://www.w3.org/2000/svg" className="rising-wave" viewBox="0 0 1440 320">
         <defs>
           <linearGradient id="myGradient" gradientTransform="rotate(0)">
             <stop offset="0%" stopColor="#11253b" />
@@ -122,7 +122,7 @@ export const IndexPageTemplate = ({
           fillOpacity="1"
           d="M0,192L80,181.3C160,171,320,149,480,160C640,171,800,213,960,224C1120,235,1280,213,1360,202.7L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
         ></path>
-      </svg>
+      </svg> */}
     </section>
 
     <section className={"hero is-medium is-dark wave-container"} id="video">
@@ -165,86 +165,6 @@ export const IndexPageTemplate = ({
         ></path>
       </svg>
     </section>
-
-    {/* <section className={"hero is-medium is-light"}>
-      <div className={"hero-body"}>
-        <div className={"container has-text-centered"}>
-          <h1 className={"title is-size-1-tablet has-margin-bottom-50"}>
-            What's your biggest challenge?
-          </h1>
-          <p className="subtitle">
-            Let us show you a pain free way to grow your business.
-          </p>
-          <div className={"columns"}>
-            <div className={"column is-half-tablet"}>
-              <article className={"is-child notification box-shadow is-white"}>
-                <h2 className={"title is-size-3"}>Operational Complexity</h2>
-                <p className={"subtitle"}>
-                  So much complexity! Keeping track of the problems is a 24 hour
-                  a day, 365 days a year headache.
-                </p>
-                <Link to="/tags/operations" className={"button is-secondary"}>
-                  I Feel the Pain &#10511;
-                </Link>
-              </article>
-            </div>
-            <div className={"column is-half-tablet"}>
-              <article className={"is-child notification box-shadow is-white"}>
-                <h2 className={"title is-size-3"}>Pain Point #2</h2>
-                <p className={"subtitle"}>
-                  Lots of problems and excruciating pain that we can solve with
-                  something clever like our owner leads funnel.
-                </p>
-                <Link to="/tags/growth" className={"button is-secondary"}>
-                  Learn More &#10511;
-                </Link>
-              </article>
-            </div>
-          </div>
-          <div className={"columns"}>
-            <div className={"column is-half-tablet"}>
-              <article className={"is-child notification box-shadow is-white"}>
-                <h2 className={"title is-size-3"}>Pain Point #3</h2>
-                <p className={"subtitle"}>
-                  Lots of problems and excruciating pain that we can solve with
-                  something clever like our direct bookings sites.
-                </p>
-                <Link
-                  to="/tags/direct-bookings"
-                  className={"button is-secondary"}
-                >
-                  Learn More &#10511;
-                </Link>
-              </article>
-            </div>
-            <div className={"column is-half-tablet"}>
-              <article className={"is-child notification box-shadow is-white"}>
-                <h2 className={"title is-size-3"}>Pain Point #4</h2>
-                <p className={"subtitle"}>
-                  Lots of problems and excruciating pain that we can solve with
-                  something clever like our owner workflow tools.
-                </p>
-                <Link to="/tags/workflows" className={"button is-secondary"}>
-                  Learn More &#10511;
-                </Link>
-              </article>
-            </div>
-          </div>
-        </div>
-      </div>
-      <svg xmlns="http://www.w3.org/2000/svg" className="rising-wave" viewBox="0 0 1440 320">
-        <path
-          fill="#ffffff"
-          fillOpacity="0.9"
-          d="M0,192L80,213.3C160,235,320,277,480,272C640,267,800,213,960,192C1120,171,1280,181,1360,186.7L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
-        ></path>
-        <path
-          fill="#ffffff"
-          fillOpacity="0.5"
-          d="M0,224L80,213.3C160,203,320,181,480,192C640,203,800,245,960,272C1120,299,1280,309,1360,314.7L1440,320L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
-        ></path>
-      </svg>
-    </section> */}
 
     {highlights && highlights.length ? (
       <div>
